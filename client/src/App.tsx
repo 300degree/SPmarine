412f62a51f2a646421fcd241cf894f72ac4ab1df
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2025-02-14 20:46:40 +0700

    setup-reactrouter-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import BargePage from './pages/BargePage'
+import OrderPage from './pages/OrderPage'
+import TugboatPage from './pages/TugboatPage'
 
 type Props = {}
 
-export default function App({ }: Props) {
-    return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+export default function App({}: Props) {
+  return (
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path='TugboatPage' element={<TugboatPage />} />
+          <Route path='OrderPage' element={<OrderPage />} />
+          <Route path='BargePage' element={<BargePage />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  )
 }
