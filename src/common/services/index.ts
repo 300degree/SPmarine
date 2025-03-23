0aa6d021a9baee1484ac495c454a997540b34557
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2025-03-23 18:39:58 +0700

    [FIX] remove comment

--- src/common/services/index.ts
+++ src/common/services/index.ts
@@ -1,5 +1,1 @@
-/* This line of code `export * from './httpClient';` is exporting all named exports from the
-`httpClient` module. It allows you to re-export all named exports from another module, making them
-available for import in the current module. This can be useful for creating a single entry point for
-multiple modules or for organizing your codebase. */
 export * from './httpClient';
