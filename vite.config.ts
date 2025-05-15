4cc40d0342813da59a4f54640efa9dde11adf369
300degree <kitibodee05p@gmail.com>
2025-05-15 10:37:36 +0000

    default host to localhost

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,19 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import path from "node:path";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import path from 'node:path';
 
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
-		alias: [{ find: "@", replacement: "/src" }],
+		alias: [{ find: '@', replacement: '/src' }],
 	},
 	server: {
-		host: "0.0.0.0",
+		host: '0.0.0.0',
 		port: Number(process.env.PORT) | 18000,
 	},
 	define: {
-		__API_ENDPOINT__: JSON.stringify(
-			process.env.__API_ENDPOINT__ || "http://62.72.30.12:18001",
-		),
-		__API_VERSION__: JSON.stringify(process.env.__API_VERSION__ || "v1"),
-		__PUBLIC_ENDPOINT__: JSON.stringify(path.join(__dirname, "./public")),
+		__API_ENDPOINT__: JSON.stringify(process.env.__API_ENDPOINT__ || 'http://localhost:18001'),
+		__API_VERSION__: JSON.stringify(process.env.__API_VERSION__ || 'v1'),
+		__PUBLIC_ENDPOINT__: JSON.stringify(path.join(__dirname, './public')),
 	},
 });
